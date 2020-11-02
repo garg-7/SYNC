df900ee3540930ff01e45855d6d4fe5e0c7d8e93
vyaskartik20 <vyas.2@iitj.ac.in>
2020-11-02 20:52:31 +0530

    song transfer added

--- file/client1/client.py
+++ file/client1/client.py
@@ -1,9 +1,10 @@
 import socket
 import pygame
 import json
+import os
 
 pygame.mixer.init()
-pygame.mixer.music.load('stay.mp3')
+# pygame.mixer.music.load('stay.mp3')
 
 s = socket.socket()
 host = input(str("Please enter the host address of the sender : "))
@@ -12,9 +13,30 @@ s.connect((host, port))
 
 # serverSocket=s.recv(1024).decode()
 # print(s.recv(1024).decode())
+
+songInput = input('What song you want to suggest to be played ..')
+s.send(songInput.encode())
+
+songSelect = s.recv(100).decode()
+
+if(os.path.isfile(songSelect)) :
+    songPresent='yes'
+    s.send(songPresent.encode()) 
+    print('Song is already present')
+else : 
+    songPresent='no'
+    s.send(songPresent.encode())
+    print('Song is not present, transferring ...')
+    filename = songSelect
+    file = open(filename, 'wb')
+    file_data = s.recv(110241024)
+    file.write(file_data)
+    file.close()
+    print('Song transferred to the machine successfully')
+    
+pygame.mixer.music.load(songSelect)
 while True:
-    songInput = input('What song you want to suggest to be played ..')
-    s.send(songInput.encode())
+    
     received = s.recv(100).decode()
     if received == 'play':
         print("Starting playback...")
--- file/server/server.py
+++ file/server/server.py
@@ -3,9 +3,10 @@ from _thread import start_new_thread
 import pygame
 import json
 import time
+import os
 
 pygame.mixer.init()
-pygame.mixer.music.load('stay.mp3')
+# pygame.mixer.music.load('stay.mp3')
 
 s = socket.socket()
 host = socket.gethostname()
@@ -17,9 +18,12 @@ s.listen()
 def doOpn(clientsocket, cmd):
     clientsocket.send(cmd.encode())
 
+networkCount=input('How many other devices do you want to connect?')
+networkCount=int(networkCount)
+
 ID = 0
 clients = []
-while ID<1:
+while ID<networkCount:
     clientsocket, address = s.accept()
     clients.append(clientsocket)
     # clientsocket.send(s.encode())
@@ -27,14 +31,31 @@ while ID<1:
     print(f"Connected with {address}, allotted ID={ID}")
     ID+=1
 
+for c in clients:
+    songs=c.recv(1024).decode()
+    print (songs)
+    
+songSelect = input('Enter the song to be played (followed by the file extension)')
+pygame.mixer.music.load(songSelect)
+
 while True:
+    for c in clients:
+        c.send(songSelect.encode())
+        songPresent=c.recv(1024).decode()
+        if(songPresent == 'yes') :
+            print('song is present in ' , c)
+        else : 
+            print('transferring song to ' , c)
+            file = open(songSelect , 'rb')
+            file_data = file.read(110241024)
+            c.send(file_data)
+            print('song transferred to ' , c)
+    
+    cmd = input('What do you want to do? (play/pause/resume/stop/end)')    
     
     for c in clients:
-        songs=c.recv(1024).decode()
-        print (songs)
-    cmd = input('What do you want to do? (play/pause/resume/stop/end)')
-    for c in clients:
         start_new_thread(doOpn, (c, cmd))
+    
     if cmd == 'play':
         print("Starting playback...")
         pygame.mixer.music.play(1)
